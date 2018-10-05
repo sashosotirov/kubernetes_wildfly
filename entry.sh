9ee821ec5f5468036e8ca937ab2de8d7b51cd5d2
sashosotirov <sotirov.sasho@gmail.com>
2018-10-05 17:08:50 +0300

    correction it message for your changes. Lines starting

--- entry.sh
+++ entry.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 ln -s $KEYPATH $WILDFLY_HOME/standalone/configuration/certificates.jks 
 $WILDFLY_HOME/bin/add-user.sh $WILDFLY_USER $WILDFLY_PASSWORD
+$WILDFLY_HOME/bin/add-user.sh -a $WILDFLY_USER $WILDFLY_PASSWORD
 $WILDFLY_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 $@
+
 
 
 
